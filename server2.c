059bc40b51c3050099713722d0131f44337d00ca
KARENKAMORI <74088988+KARENKAMORI@users.noreply.github.com>
2024-06-25 11:15:59 +0300

    Update server2.c

--- server2.c
+++ server2.c
@@ -101,7 +101,7 @@ int main() {
         process_request(buffer, client_addr, sockfd);
 
         // Close the client socket
-        close(client_addr);
+        close(sockaddr_in);
     }
 
     close(sockfd);
